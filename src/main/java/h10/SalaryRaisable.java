8e0d3e32e1dd71a350486d30d0e1fef37f22bb0e
bramjanssens <bram.janssens@infosupport.com>
2020-03-31 10:01:29 +0200

    Interface additional features added.

--- src/main/java/h10/SalaryRaisable.java
+++ src/main/java/h10/SalaryRaisable.java
@@ -1,5 +1,14 @@
 package h10;
 
 public interface SalaryRaisable {
-    void raiseSalary(int amount);
+
+    public abstract void raiseSalary(int amount);
+
+    public static final int amount = 10;
+
+    // since java 8
+    default void cutSalary(int amount) {
+        // doe iets defaults met amount
+        throw new RuntimeException("Not implemented...");
+    }
 }
