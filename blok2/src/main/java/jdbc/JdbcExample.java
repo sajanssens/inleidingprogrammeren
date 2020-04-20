65b7f82e7ec0935fef6a1050a9abbbadadc68966
bramjanssens <bram.janssens@infosupport.com>
2020-04-20 11:11:59 +0200

    JdbcExample added.

---
+++ blok2/src/main/java/jdbc/JdbcExample.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.sql.*;
+
+public class JdbcExample {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        // 1
+        // Class.forName("com.mysql.cj.jdbc.Driver"); // not necessary since Java 6; now via the SPI
+
+        // 2
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdemo", "root", "root");
+
+        // 3
+        Statement statement = connection.createStatement();
+
+        // 4
+        ResultSet result = statement.executeQuery("SELECT * FROM PERSON");
+
+        // 5
+        while (result.next()) {
+            String name = result.getString("name");
+            int age = result.getInt("age");
+
+            System.out.println(name + " - " + age);
+        }
+
+        // 6
+        statement.close();
+        connection.close();
+    }
+}
