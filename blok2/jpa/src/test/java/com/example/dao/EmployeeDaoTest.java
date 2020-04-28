0f259d6920ab994038dcba58a8f63b62921d9995
bramjanssens <bram.janssens@infosupport.com>
2020-04-28 15:17:13 +0200

    EmployeeDaoTest improved.

--- blok2/jpa/src/test/java/com/example/dao/EmployeeDaoTest.java
+++ blok2/jpa/src/test/java/com/example/dao/EmployeeDaoTest.java
@@ -11,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.*;
 
@@ -23,16 +24,12 @@ class EmployeeDaoTest {
     @InjectMocks
     EmployeeDao dao = new EmployeeDao();
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void whenInsertIsCalledATransactionIsBegunAndPersistIsCalledAndCommitted() {
+        // given
         when(emMock.getTransaction()).thenReturn(entityTransactionMock);
         doNothing().when(entityTransactionMock).begin();
         doNothing().when(entityTransactionMock).commit();
-    }
-
-    @Test
-    void insert() {
-        // given: see setup
 
         // when
         dao.insert(new Employee());
@@ -43,5 +40,24 @@ class EmployeeDaoTest {
         verify(emMock, atLeastOnce()).getTransaction();
         verify(entityTransactionMock).begin();
         verify(entityTransactionMock).commit();
+    }
+
+    @Test
+    void whenSelectIsCalledFindIsExecutedWithoutTransactionAndResultIsCorrect() {
+        // given
+        Employee mock = mock(Employee.class);
+        when(emMock.find(any(), anyLong())).thenReturn(mock);
+
+        // when
+        Employee select = dao.select(1L);
+
+        // then
+        verify(emMock).find(any(), eq(1L));
+
+        verify(emMock, never()).getTransaction();
+        verify(entityTransactionMock, never()).begin();
+        verify(entityTransactionMock, never()).commit();
+
+        assertThat(select).isEqualTo(mock);
     }
 }
