a2f9b2fed1a00f31ca0c17e48c710dc5c07e8376
bramjanssens <bram.janssens@infosupport.com>
2020-04-29 11:22:54 +0200

    CDI added (2).

--- blok2/jpa/src/main/java/com/example/App.java
+++ blok2/jpa/src/main/java/com/example/App.java
@@ -1,92 +1,19 @@
 package com.example;
 
-import com.example.dao.EmployeeDao;
-import com.example.domain.*;
-import org.slf4j.Logger;
-
-import javax.persistence.EntityManager;
-import java.util.List;
-
-import static com.example.util.Util.logger;
-import static com.example.util.Util.mysql;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
 
 public class App {
 
-    private final Logger log = logger(getClass());
-
-    // application managed persistence context
-    private EntityManager em = mysql();
-
     public static void main(String[] args) {
-        new App().start();
+        // CDI < 2.0 (JEE7)
+        // Container is hard coded to be Weld.
+        Weld weld = new Weld();
+        WeldContainer container = weld.initialize();
+
+        // EmployeeService employeeService = new EmployeeService(); // oude stijl
+        EmployeeService employeeService = container.select(EmployeeService.class).get();
+        employeeService.start();
     }
-
-    private void start() {
-        log("Starting app...");
-
-        EmployeeDao employeeDao = new EmployeeDao(em);
-        Employee bram = new Employee("Bram", 42);
-        // bram.setEmailaddress("bram@test.com");
-
-        employeeDao.insert(bram);
-
-        Employee employee = employeeDao.select(1);
-        log(employee);
-
-        employeeDao.selectAll().forEach(this::log);
-
-        List<Employee> brams = employeeDao.selectAll("Bram");
-        brams.forEach(this::log);
-
-        Employee p = new Employee("", 1);
-        employeeDao.insert(p);
-        employeeDao.delete(p.getId());
-        employeeDao.selectAll().forEach(this::log);
-
-        Employee nino = employeeDao.updateFirstname(1, "Gert Jan");
-        employeeDao.selectAll().forEach(this::log);
-
-        nino.setNaam("Nino");
-        Employee updated = employeeDao.update(nino);
-
-        employeeDao.selectAllNamed().forEach(this::log);
-
-        updated.setBossOfDepartment(new Department("Nergens"));
-        updated = employeeDao.update(updated);
-
-        updated.addLaptop(new Laptop("DELL"));
-        updated = employeeDao.update(updated);
-
-        updated.setLeaseCar(new Car("Skoda"));
-        employeeDao.update(updated);
-
-        updated.setParkingSpace(new ParkingSpace(123));
-        employeeDao.update(updated);
-
-        updated.addPhone(new Phone("+316123456789"));
-        employeeDao.update(updated);
-
-        updated.addDepartment(new Department("Douane"));
-        updated.addDepartment(new Department("Toeslagen"));
-        employeeDao.update(updated);
-
-        employeeDao.findEmployeeDepartments().forEach(this::log);
-
-        List<Employee> ninos = employeeDao.findUsingCriteriaAPI("Nino", "");
-        ninos.forEach(this::log);
-
-        TemporaryEmployee temporaryEmployee = new TemporaryEmployee("Tempie", 56, 5);
-        employeeDao.insert(temporaryEmployee);
-        employeeDao.selectAll().forEach(this::log); // includes sub classes
-
-        log(isManaged(updated));
-        em.clear();
-        log(isManaged(updated));
-        em.close();
-    }
-
-    private void log(Object o) { log.info(o + ""); }
-
-    private boolean isManaged(Employee p) { return em.contains(p); }
 
 }
---
+++ blok2/jpa/src/main/java/com/example/EmployeeService.java
@@ -0,0 +1,92 @@
+package com.example;
+
+import com.example.dao.EmployeeDao;
+import com.example.domain.*;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class EmployeeService {
+
+    @Inject
+    private Logger log;
+
+    // application (weld) managed persistence context
+    @Inject
+    @Named("mysql")
+    private EntityManager em;
+
+    @Inject
+    private EmployeeDao employeeDao;
+
+    public void start() {
+        log("Starting app...");
+
+        Employee bram = new Employee("Bram", 42);
+        // bram.setEmailaddress("bram@test.com");
+
+        employeeDao.insert(bram);
+
+        Employee employee = employeeDao.select(1);
+        log(employee);
+
+        employeeDao.selectAll().forEach(this::log);
+
+        List<Employee> brams = employeeDao.selectAll("Bram");
+        brams.forEach(this::log);
+
+        Employee p = new Employee("", 1);
+        employeeDao.insert(p);
+        employeeDao.delete(p.getId());
+        employeeDao.selectAll().forEach(this::log);
+
+        Employee nino = employeeDao.updateFirstname(1, "Gert Jan");
+        employeeDao.selectAll().forEach(this::log);
+
+        nino.setNaam("Nino");
+        Employee updated = employeeDao.update(nino);
+
+        employeeDao.selectAllNamed().forEach(this::log);
+
+        updated.setBossOfDepartment(new Department("Nergens"));
+        updated = employeeDao.update(updated);
+
+        updated.addLaptop(new Laptop("DELL"));
+        updated = employeeDao.update(updated);
+
+        updated.setLeaseCar(new Car("Skoda"));
+        employeeDao.update(updated);
+
+        updated.setParkingSpace(new ParkingSpace(123));
+        employeeDao.update(updated);
+
+        updated.addPhone(new Phone("+316123456789"));
+        employeeDao.update(updated);
+
+        updated.addDepartment(new Department("Douane"));
+        updated.addDepartment(new Department("Toeslagen"));
+        employeeDao.update(updated);
+
+        employeeDao.findEmployeeDepartments().forEach(this::log);
+
+        List<Employee> ninos = employeeDao.findUsingCriteriaAPI("Nino", "");
+        ninos.forEach(this::log);
+
+        TemporaryEmployee temporaryEmployee = new TemporaryEmployee("Tempie", 56, 5);
+        employeeDao.insert(temporaryEmployee);
+        employeeDao.selectAll().forEach(this::log); // includes sub classes
+
+        log(isManaged(updated));
+        em.clear();
+        log(isManaged(updated));
+        em.close();
+    }
+
+    private void log(Object o) { log.info(o + ""); }
+
+    private boolean isManaged(Employee p) { return em.contains(p); }
+
+}
