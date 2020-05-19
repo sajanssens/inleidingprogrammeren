3ed41da78baee6e1c38547d1973194e0ad220e3e
bramjanssens <bram.janssens@infosupport.com>
2020-05-19 11:09:29 +0200

    More on REST.

--- blok4/jee7/rest/src/main/java/com/example/resources/StudentsResource.java
+++ blok4/jee7/rest/src/main/java/com/example/resources/StudentsResource.java
@@ -2,32 +2,29 @@ package com.example.resources;
 
 import com.example.domain.Student;
 import com.example.domain.Students;
-import com.example.services.StudentService;
 
-import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.ws.rs.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.example.domain.Values.JANSSENS;
+import static com.example.domain.Values.STUDENTS;
 import static java.util.stream.Collectors.toList;
 
 @Path("/students")
 @Produces("application/json")
-@SessionScoped // therefore this bean needs to be serializable
+// @SessionScoped // therefore this bean needs to be serializable
 public class StudentsResource implements Serializable {
 
     List<Student> students = new ArrayList<>();
 
-    @Inject
-    private StudentService studentService;
+    // @Inject
+    // private StudentService studentService;
 
     @GET
     @Path("{id}")
     public Student get(@PathParam("id") int id) {
-        return JANSSENS;
+        return STUDENTS.get(id);
     }
 
     @GET
@@ -36,28 +33,29 @@ public class StudentsResource implements Serializable {
         return filterStudentsBy(lastname);
     }
 
-    @GET
-    @Path("name/{lastname}")
-    public Students getByName(@QueryParam("lastname") String lastname) {
-        return Students.of(filterStudentsBy(lastname));
+    // @GET
+    // @Path("name/{lastname}")
+    // public Students getByName(@QueryParam("lastname") String lastname) {
+    //     return Students.of(filterStudentsBy(lastname));
+    // }
+
+    @POST
+    public Student post(Student student) {
+        students.add(student); // doesn't make sense; rest is stateless
+        return student;
+        // if (studentService.add(student)) {
+        //     return student;
+        // }
+        // throw new RuntimeException("Student not added.");
     }
 
     @GET
     public Students getAll() {
-        // return students; // doesn't make sense; rest is stateless
-        return studentService.getAll();
+        return Students.of(students); // doesn't make sense; rest is stateless
+        // return studentService.getAll();
     }
 
-    @POST
-    public Student post(Student student) {
-        // students.add(student); // doesn't make sense; rest is stateless
-        if (studentService.add(student)) {
-            return student;
-        }
-        throw new RuntimeException("Student not added.");
-    }
-
-    private List<Student> filterStudentsBy(@QueryParam("lastname") String lastname) {
+    private List<Student> filterStudentsBy(String lastname) {
         return students.stream()
                 .filter(s -> lastname.equals(s.getLastname()))
                 .collect(toList());
